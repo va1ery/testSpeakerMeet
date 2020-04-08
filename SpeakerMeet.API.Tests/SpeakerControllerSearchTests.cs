3edff59b1b0dec64e21ad3d7de3210451055d4cb
va1ery <valery.gourov@gmail.com>
2020-04-08 10:18:46 +0300

    first commit

---
+++ SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
@@ -0,0 +1,15 @@
+        using System;
+        using Xunit;
+        using SpeakerMeet.API.Controllers;   //<-- directive to add
+
+        namespace SpeakerMeet.API.Tests
+        {
+            public class SpeakerControllerSearchTests  //<-- to rename
+            {
+                [Fact]
+                public void ItExists()
+                {
+                var controller = new SpeakerController();
+                }
+            }
+        }
---
+++ SpeakerMeet.API/Controllers/SpeakerController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpeakerMeet.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class SpeakerController : Controller
+    {
+    }
+}    
